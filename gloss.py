1a46b6b9bca708f09b025530cd422fe328b8ad10
Onanii Yui <177497261+yui-onanii@users.noreply.github.com>
2024-12-06 14:28:28 +0900

    gloss: move everything into main()
    Signed-off-by: Onanii Yui <177497261+yui-onanii@users.noreply.github.com>

--- gloss.py
+++ gloss.py
@@ -2,21 +2,25 @@
 
 """gloss chinese sentences in pinyin, word by word"""
 
-import sys
-import readline
-
 import common
 
-def PS():
-	"""print an input prompt"""
-	print('> ', end='', flush=True)
+def main():
+	def PS():
+		"""print an input prompt"""
+		print('> ', end='', flush=True)
 
-PS()
-for line in sys.stdin:
-	txt = line.strip()
-	words = common.phrase2words(txt)
-	for word in words:
-		py = common.word2pinyin(word)
-		# example: 你好 nǐ hǎo
-		print(word, *py)
 	PS()
+	for line in sys.stdin:
+		txt = line.strip()
+		words = common.phrase2words(txt)
+		for word in words:
+			py = common.word2pinyin(word)
+			# example: 你好 nǐ hǎo
+			print(word, *py)
+		PS()
+
+if __name__ == '__main__':
+	import sys
+	import readline
+
+	main()
